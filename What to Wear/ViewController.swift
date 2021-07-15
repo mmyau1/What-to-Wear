96ca42768f2c3d4dac3ab6609a5b7baaa47b695f
mmyau1 <86981889+mmyau1@users.noreply.github.com>
2021-07-15 12:50:07 -0500

    random Shoe Picker
    ImageView
    Random number generator
    button

--- What to Wear/ViewController.swift
+++ What to Wear/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var shoeArray = ["shoe1", "shoe2", "shoe3", "shoe4", "shoe5"]
+    var randomShoeNum = 0
+    @IBOutlet weak var shoeImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func chooseMyShoes(_ sender: Any) {
+        randomShoeNumber()
+    }
+    func randomShoeNumber() {
+        randomShoeNum = Int.random(in: 0...4)
+        shoeImage.image = UIImage(named: shoeArray[randomShoeNum])
+    }
+   
 }
 
