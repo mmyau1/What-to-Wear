36b7679e24465e5005cd51523d8b368d68f6ff46
mmyau1 <86981889+mmyau1@users.noreply.github.com>
2021-07-15 12:13:46 -0500

    Initial Commit

---
+++ What to Wear/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  What to Wear
+//
+//  Created by Melanie Yau on 7/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
